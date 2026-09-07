ee79c2c6ea9cc4b992d9beee6a560285cc1251c6
oscararrise <oscar.jimenez@arrise.com>
2026-09-07 14:05:32 -0500

    fix: restore Django template backend path

--- config/settings.py
+++ config/settings.py
@@ -35,7 +35,7 @@ ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
-        "BACKEND": "django.db.backends.django.DjangoTemplates",
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
